cf6b7fcab8b1034345dde960daec661aae3e28e3
Tejas Gupta <42479422+tejasgpt@users.noreply.github.com>
2019-07-17 02:07:45 -0400

    14. Longest Common Prefix
    SUBMISSION DETAILS
    Runtime: 12 ms, faster than 98.74% of Python online submissions for Longest Common Prefix.
    Memory Usage: 11.8 MB, less than 57.25% of Python online submissions for Longest Common Prefix.

--- Week1/Simple String Manipulation/longest-common-prefix.py
+++ Week1/Simple String Manipulation/longest-common-prefix.py
@@ -7,6 +7,30 @@ class Solution(object):
         :type strs: List[str]
         :rtype: str
         """
+        if not strs: 
+            return ""
+        elif len(strs) == 1: 
+            return strs[0]
+        
+        prefix = strs[0]
+        for i in range(1, len(strs)):
+            pre = min(len(prefix), len(strs[i]))
+            while strs[i][:pre] != prefix[:pre]: 
+                pre -= 1
+            prefix = prefix[:pre]
+        return prefix
+
+# ALTERNATE SOLUTION
+
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        PROBLEM STATEMENT:
+        Write a function to find the longest common prefix string amongst an array of strings.
+        If there is no common prefix, return an empty string "".
+        :type strs: List[str]
+        :rtype: str
+        """
         prefix = ""
         if len(strs) == 0 : 
             return prefix
