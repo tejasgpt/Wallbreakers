939e9359121a16cecd4465f393f9976fa230d146
Tejas Gupta <42479422+tejasgpt@users.noreply.github.com>
2019-07-28 22:50:38 -0400

    225. Implement Stack using Queues
    SUBMISSION DETAILS
    Runtime: 16 ms, faster than 71.73% of Python online submissions for Implement Stack using Queues.
    Memory Usage: 11.7 MB, less than 69.49% of Python online submissions for Implement Stack using Queues.

--- Week 4/Queues/implement-stack-using-queues.py
+++ Week 4/Queues/implement-stack-using-queues.py
@@ -1,10 +1,11 @@
 class MyStack(object):
-
+    
     def __init__(self):
         """
         Initialize your data structure here.
         """
-        self.queue = collections.deque()
+        self.queue1 = []
+        self.queue2 = []
 
     def push(self, x):
         """
@@ -12,31 +13,31 @@ class MyStack(object):
         :type x: int
         :rtype: None
         """
-        self.queue.append(x)
+        self.queue2.insert(0,x)
+        while self.queue1:
+            self.queue2.insert(0, self.queue1.pop())
+        self.queue1, self.queue2 = self.queue2, self.queue1
 
     def pop(self):
         """
         Removes the element on top of the stack and returns that element.
         :rtype: int
         """
-        for _ in range(len(self.queue) - 1):
-            self.queue.append(self.queue.popleft())
-
-        return self.queue.popleft()
+        return self.queue1.pop()
 
     def top(self):
         """
         Get the top element.
         :rtype: int
         """
-        return self.queue[-1]
+        return self.queue1[-1]
 
     def empty(self):
         """
         Returns whether the stack is empty.
         :rtype: bool
         """
-        return len(self.queue) == 0
+        return not self.queue1
 
 
 # Your MyStack object will be instantiated and called as such:
